46f64bfedfcb3b6f249b6e07dbed96a18084539b
Daniel N <danielxnordkvist@hotmail.com>
2018-09-12 14:58:51 +0200

    rensat

--- ConsoleApp1/Item.cs
+++ ConsoleApp1/Item.cs
@@ -5,13 +5,10 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
-
-
+        
         public override string ToString()
         {
-            
             return string.Format("Id:{0}, Name:{1}, Iscompelte:{2}", Id, Name, IsComplete);
-
         }
 
     }
